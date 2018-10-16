e9f7fc0e4c80b61abc8f2c0d1aa99d31a6c02016
alex <sokolov@ozero.net>
2018-10-16 21:24:19 +0300

    onClick methods declared

--- app/src/main/java/net/ozero/alarmservicepractice/MainActivity.java
+++ app/src/main/java/net/ozero/alarmservicepractice/MainActivity.java
@@ -2,6 +2,7 @@ package net.ozero.alarmservicepractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void set10secButtonClicked(View view) {
+    }
+
+    public void set1minButtonClicked(View view) {
+    }
+
+    public void set5minButtonClicked(View view) {
     }
 }
