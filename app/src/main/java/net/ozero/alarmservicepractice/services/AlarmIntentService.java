c5a79034cdea2578db808ba06bc89f13bb9a35d0
alex <sokolov@ozero.net>
2018-10-16 22:09:31 +0300

    JobScheduler initialized and implemented

--- app/src/main/java/net/ozero/alarmservicepractice/services/AlarmIntentService.java
+++ app/src/main/java/net/ozero/alarmservicepractice/services/AlarmIntentService.java
@@ -1,9 +1,14 @@
 package net.ozero.alarmservicepractice.services;
 
 import android.app.IntentService;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
+import net.ozero.alarmservicepractice.MainActivity;
 
 public class AlarmIntentService extends IntentService {
 
@@ -15,6 +20,8 @@ public class AlarmIntentService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
 
+        long timeMillis = intent.getLongExtra(MainActivity.EXTRA_KEY_TIME, 1000);
+        scheduleJob(timeMillis);
     }
 
     @Override
@@ -31,5 +38,24 @@ public class AlarmIntentService extends IntentService {
         Log.i(getClass().getName(), "onDestroy");
 
         super.onDestroy();
+    }
+
+    private void scheduleJob(long timeMillis) {
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        JobInfo job =
+                new JobInfo.Builder(1, new ComponentName(this, AlarmJobService.class))
+                        .setMinimumLatency(timeMillis)
+                        .setOverrideDeadline(timeMillis)
+                        .setBackoffCriteria(5*1000, JobInfo.BACKOFF_POLICY_LINEAR)
+                        .setPersisted(true)
+                        .build();
+
+        if (jobScheduler != null) {
+            jobScheduler.schedule(job);
+            Log.i(getClass().getName(), "job set");
+        } else {
+            Log.i(getClass().getName(), "scheduler = null!");
+        }
     }
 }
